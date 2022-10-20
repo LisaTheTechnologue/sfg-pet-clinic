688cc51eb8ebe900e57e3689e9e306014ca125c8
LisaTheTechnologue <jolisa.chan.store@gmail.com>
2022-10-20 11:23:01 +0700

    added specialties map impl . closes #31

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/fsoft/spring/sfgpetclinic/services/SpecialtyService.java
@@ -0,0 +1,9 @@
+package fsoft.spring.sfgpetclinic.services;
+
+import fsoft.spring.sfgpetclinic.model.Specialty;
+
+/**
+ * Created by jt on 7/31/18.
+ */
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/fsoft/spring/sfgpetclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,41 @@
+package fsoft.spring.sfgpetclinic.services.map;
+
+import fsoft.spring.sfgpetclinic.model.Specialty;
+import fsoft.spring.sfgpetclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created by jt on 7/31/18.
+ */
+@Service
+@Profile({"default", "map"})
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
